26d8c07b78900878d14f4c598f2637fb05f50f17
= <lukasilverio94@gmail.com>
2025-03-10 16:33:46 +0100

    add: using comparable interface to order by name or salary

---
+++ interfaces_comparable/src/entities/Employee.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Employee implements Comparable<Employee> { // add Comparable so on main we can use Collections.sort();
+
+	private String name;
+	private Double salary;
+
+	public Employee(String name, Double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int compareTo(Employee other) {
+		return -salary.compareTo(other.getSalary());
+	}
+
+}
