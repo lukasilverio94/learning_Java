1a7d8c4a4f0c5b88ca68e2f92e89037d08f30916
= <lukasilverio94@gmail.com>
2025-03-17 23:11:47 +0100

    add pipeline streams example

--- functional_programming_streams/src/application/CreateStreams.java
+++ functional_programming_streams/src/application/CreateStreams.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Program {
+public class CreateStreams {
 	public static void main(String[] args) {
 
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
