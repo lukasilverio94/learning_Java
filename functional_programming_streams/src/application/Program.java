089abe7582962c330492f61f6342254bb6a53ecc
= <lukasilverio94@gmail.com>
2025-03-17 21:51:22 +0100

    creating streams examples

---
+++ functional_programming_streams/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Program {
+	public static void main(String[] args) {
+
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 17);
+		
+		// first stream (list.stream() )
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
+		System.out.println(Arrays.toString(st1.toArray()));
+		
+		// second stream (Stream.of)
+		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
+		
+		// Iteration function stream with short-circuit (limit)
+		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+		
+		// Fibonnaci sequence stream
+		Stream<Long> fibonacci = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0]+p[1]}).map(p -> p[0]);
+		System.out.println(Arrays.toString(fibonacci.limit(10).toArray()));
+	}
+}
