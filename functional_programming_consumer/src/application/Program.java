8161e7e91e95352171b730aea411c631d19ed25b
= <lukasilverio94@gmail.com>
2025-03-17 13:19:13 +0100

    version3: using nonStatic method on product to apply on forEach

--- functional_programming_consumer/src/application/Program.java
+++ functional_programming_consumer/src/application/Program.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Product;
-import util.PriceUpdate;
 
 public class Program {
 	public static void main(String[] args) {
@@ -15,7 +14,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::staticPriceUpdate);
+		list.forEach(Product::nonStaticPriceUpdate);
 		
 		list.forEach(System.out::println);
 		
--- functional_programming_consumer/src/entities/Product.java
+++ functional_programming_consumer/src/entities/Product.java
@@ -32,7 +32,13 @@ public class Product {
 	public static void staticPriceUpdate(Product p) {
 		p.setPrice(p.getPrice() * 1.1);
 	}
-	
+
+	public void nonStaticPriceUpdate() {
+     //	setPrice(getPrice() * 1.1);
+		// OR
+		price *= 1.1;
+	}
+
 	@Override
 
 	public String toString() {
