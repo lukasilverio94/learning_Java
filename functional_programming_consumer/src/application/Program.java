486abb3b113c9bc28fd592abf422323b60c8767d
= <lukasilverio94@gmail.com>
2025-03-17 13:23:47 +0100

    version4: declared lambda function

--- functional_programming_consumer/src/application/Program.java
+++ functional_programming_consumer/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -14,7 +15,10 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1; // we will apply this to increase 10% to all products
+		
+		Consumer<Product> consumer = p -> p.setPrice(p.getPrice() * factor);
+		list.forEach(consumer);
 		
 		list.forEach(System.out::println);
 		
